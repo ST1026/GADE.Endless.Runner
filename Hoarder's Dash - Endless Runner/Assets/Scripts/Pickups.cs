d78df5767f75140dde8283df5ace68ce75e53ebc
ST1026 <st10260942@vgconnect.edu.za>
2026-03-26 01:21:00 +0200

    Movement mostly sorted, game over screen /restarting sorted, working on pick up

---
+++ Hoarder's Dash - Endless Runner/Assets/Scripts/Pickups.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class Pickups : MonoBehaviour
+{
+
+    public float rotateSpd = 100f;
+
+    
+    void Update()
+    {
+        //make pickups spin
+        transform.Rotate(Vector3.up * rotateSpd * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            FindAnyObjectByType <ObstacleSpawner>().ScoreCoins();
+            Destroy(gameObject);
+        }
+    }
+
+
+}
