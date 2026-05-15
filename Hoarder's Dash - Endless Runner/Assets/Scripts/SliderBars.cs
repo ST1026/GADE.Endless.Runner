bd9fb155539cbb3441271871cb633edac2b69ddf
ST1026 <st10260942@vgconnect.edu.za>
2026-05-15 14:19:06 +0200

    All that I need to work, works just fine :)

--- Hoarder's Dash - Endless Runner/Assets/Scripts/SliderBars.cs
+++ Hoarder's Dash - Endless Runner/Assets/Scripts/SliderBars.cs
@@ -4,11 +4,30 @@ using UnityEngine.UI;
 public class SliderBars : MonoBehaviour
 {
     [SerializeField] private Slider bossHealthbar;
+    [SerializeField] private Slider spdBoostbar;
+    [SerializeField] private Slider shieldBar;
+    [SerializeField] private Slider gunBar;
 
     public void UpdateHealth(float currHealth, float maxHealth)
     {
         bossHealthbar.value = currHealth/maxHealth;
     }
+
+    public void UpdateSpeedBar(float currSpeed, float maxSpeed)
+    {
+        spdBoostbar.value = currSpeed/maxSpeed;
+    }
+
+    public void UpdateShieldBar(float currShield, float maxShield)
+    {
+        shieldBar.value = currShield/maxShield;
+    }
+
+    public void UpdateGunBar(float currGun, float maxGun)
+    {
+        gunBar.value = currGun/maxGun;
+    }
+
 
     // Update is called once per frame
     void Update()
